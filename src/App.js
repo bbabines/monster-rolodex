5ee7020f2112d84964e26615bd5c9a2a8d4bdf0b
Tammy Wilson <bbabines@gmail.com>
2022-11-11 23:10:59 -0500

    Filtering typeahead

--- src/App.js
+++ src/App.js
@@ -7,31 +7,43 @@ class App extends Component {
     super();
 
     this.state = {
-      monsters: [
-        {
-        name: "Linda"
-      },
-       {
-        name: "Frank"
-      },
-      {
-        name: "Jacky"
-      }]
-    }
+      monsters: []
+    };
+    console.log("constructor");
   }
+
+  componentDidMount() {
+    console.log("componentDidMount");
+    fetch("https://jsonplaceholder.typicode.com/users")
+    .then((response) => response.json())
+     .then((users) => this.setState(() => {
+        return {monsters: users}
+      },
+      () => {
+        console.log(users);
+      }
+     ));
+  }
+     
 
 
 
   render() {
+    console.log("render");
   return (
     <div className="App">
+      <input className="search-box" type="search" placeholder="search monsters" onChange={(event)=> {
+       // Use the array method .filter to filter out the values...
+       console.log(event.target.value);
+      }} />
   {
     this.state.monsters.map( (monster) => {
-     return <h1>{monster.name}</h1>
-    }
-
-    )
-  }
+     return (
+      <div key={monster.id}>
+        <h1 >{monster.name}</h1>
+     </div>
+     )
+    })}
     </div>
   );
 }}
