cc1f49d98ad3832b58ff36a84111d56d3313acae
Brad Babines <bbabines@gmail.com>
2022-11-16 13:21:23 -0500

    Version 2.0 complete w/ Functional Components

--- monster-rolodex/my-app/src/App.js
+++ monster-rolodex/my-app/src/App.js
@@ -1,75 +1,54 @@
-import { Component } from "react";
+// useState is a Hooks used in functional components
+// useEffect is a hook using the side effect of a function
+import { useState, useEffect } from "react";
+// As apposed to setState which is for class components
 
-import "./App.css";
 import CardList from "./components/card-list/card-list.component.jsx";
 import SearchBox from "./components/search-box/search-box.component";
+import "./App.css";
 
-// const App = () => {
-// 	return (
-// 		<div className="App">
-// 			<h1 className="app-title">Monsters Rolodex</h1>
-// 			<SearchBox
-// 				className="monsters-search-box"
-// 				onChangeHandler={onSearchChange}
-// 				placeholder="search monsters"
-// 			/>
+const App = () => {
+	// [value, setValue]
+	const [searchField, setSearchField] = useState("");
+	const [monsters, setMonsters] = useState([]);
+	const [filteredMonsters, setFilterMonsters] = useState(monsters);
 
-// 			<CardList monsters={filteredMonsters} />
-// 		</div>
-// 	);
-// }
-
-class App extends Component {
-	constructor() {
-		super();
-
-		this.state = {
-			monsters: [],
-			searchField: "",
-		};
-	}
-
-	componentDidMount() {
+	// First argument is the effect we want to happen inside our fxn component. The second argument is a dependency array (state or prop values).
+	// When the second argument changes, the fxn will run again.
+	// The dependency is empty therefore we don't ever want the fxn to run again.
+	useEffect(() => {
 		fetch("https://jsonplaceholder.typicode.com/users")
 			.then((response) => response.json())
-			.then((users) =>
-				this.setState(() => {
-					return { monsters: users };
-				})
-			);
-	}
+			.then((users) => setMonsters(users));
+	}, []);
 
-	onSearchChange = (event) => {
-		// Using toLocaleLowerCase to bypass filter's case sensitive nature
-		const searchField = event.target.value.toLocaleLowerCase();
-
-		this.setState(() => {
-			return { searchField };
+	useEffect(() => {
+		const newFilteredMonsters = monsters.filter((monster) => {
+			return monster.name.toLocaleLowerCase().includes(searchField);
 		});
+
+		setFilterMonsters(newFilteredMonsters);
+	}, [monsters, searchField]);
+
+	const onSearchChange = (event) => {
+		// Using toLocaleLowerCase to bypass filter's case sensitive nature
+		const searchFieldString = event.target.value.toLocaleLowerCase();
+		setSearchField(searchFieldString);
 	};
 
-	render() {
-		const { monsters, searchField } = this.state;
-		const { onSearchChange } = this;
+	return (
+		<div className="App">
+			<h1 className="app-title">Monsters Rolodex</h1>
 
-		// Filter IS case sensitive
-		const filteredMonsters = monsters.filter((input) => {
-			return input.name.toLocaleLowerCase().includes(searchField);
-		});
+			<SearchBox
+				className="monsters-search-box"
+				onChangeHandler={onSearchChange}
+				placeholder="search monsters"
+			/>
 
-		return (
-			<div className="App">
-				<h1 className="app-title">Monsters Rolodex</h1>
-				<SearchBox
-					className="monsters-search-box"
-					onChangeHandler={onSearchChange}
-					placeholder="search monsters"
-				/>
-
-				<CardList monsters={filteredMonsters} />
-			</div>
-		);
-	}
-}
+			<CardList monsters={filteredMonsters} />
+		</div>
+	);
+};
 
 export default App;
--- monster-rolodex/my-app/src/components/card-list/card-list.component.jsx
+++ monster-rolodex/my-app/src/components/card-list/card-list.component.jsx
@@ -1,18 +1,13 @@
-import { Component } from "react";
-
 import Card from "../card/card.component";
 import "./card-list.styles.css";
 
-export default class CardList extends Component {
-	render() {
-		const { monsters } = this.props;
+// Functional components only use props and sometimes props with forwardRef
+const CardList = ({ monsters }) => (
+	<div className="card-list">
+		{monsters.map((monster) => {
+			return <Card monster={monster} />;
+		})}
+	</div>
+);
 
-		return (
-			<div className="card-list">
-				{monsters.map((monster) => {
-					return <Card monster={monster} />;
-				})}
-			</div>
-		);
-	}
-}
+export default CardList;
